dade3c0c5e27bc3fe259beea478d9b45dbe2c14e
Rodri <rodrimuan@gmail.com>
2024-03-11 22:04:51 -0500

    test unitario de python

--- tests/test.annos_bisiestos.py
+++ tests/test.annos_bisiestos.py
@@ -10,8 +10,8 @@ print(root_path)
 sys.path.append(str(root_path))
 
 from src.annos_bisiestos import evaluar
-
 class TestAnnosBisiestos(unittest.TestCase):
+    
     def test_1988(self):
         valor_esperado = "1988 es bisiesto"
         valor_actual = evaluar(1988)
@@ -20,5 +20,14 @@ class TestAnnosBisiestos(unittest.TestCase):
     # TODO: Agrega tus otros casos de prueba aquí
     
 
+    def test_1700(self):
+        valor_esperado = "1700 no es bisiesto"
+        valor_actual = evaluar(1700)
+        self.assertEqual(valor_esperado, valor_actual)
+
+    def test_2400(self):
+        valor_esperado = "2400 es bisiesto"
+        valor_actual = evaluar(2400)
+        self.assertEqual(valor_esperado, valor_actual)
 if __name__ == '__main__':
     unittest.main()
